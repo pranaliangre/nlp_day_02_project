6c5c91b89440b63a2c87bd5580909fad6cafaa4f
pranaliangre <pranali.r.c@gmail.com>
2018-03-14 17:21:26 +0000

    Done

--- q01_load_data_tfidf/build.py
+++ q01_load_data_tfidf/build.py
@@ -1,10 +1,18 @@
+
 # Default imports
-
-import pandas as pd
-from sklearn.feature_extraction.text import TfidfVectorizer
-
-# Write your solution here :
+from sklearn.datasets import load_files
+from sklearn.model_selection import train_test_split
 
 
+# Your solution here:
 
+def q01_load_data(path, seed=9):
+    'write your solution here'
+
+    categories = ['alt.atheism', 'soc.religion.christian', 'comp.graphics', 'sci.med']
+    ## Here the path is the directory and categories are the categories for which we want to load the function
+    twenty_train = load_files(path, categories)
+    X_train, X_test, y_train, y_test = train_test_split(twenty_train.data, twenty_train.target, test_size=0.8,
+                                                        random_state=seed)
+    return twenty_train, X_train, X_test, y_train, y_test
 
--- q02_count_vectorizer_for_LDA/build.py
+++ q02_count_vectorizer_for_LDA/build.py
@@ -1,3 +1,4 @@
+
 # Default imports
 
 import pandas as pd
@@ -7,6 +8,11 @@ from greyatomlib.nlp_day_02_project.q01_load_data_tfidf.build import q01_load_da
 
 from sklearn.feature_extraction.text import CountVectorizer
 
-# Write your solution here:
 
+def q02_count_vectorizer_for_LDA(path):
+    data, tfidf, tfidf_feature_names = q01_load_data_tfidf(path)
+    vectorizer = CountVectorizer(analyzer='word', ngram_range=(1, 1), min_df=0, stop_words='english')
+    matrix = vectorizer.fit_transform(data['talkTitle'])
+    feature_names = vectorizer.get_feature_names()
+    return matrix, feature_names
 
