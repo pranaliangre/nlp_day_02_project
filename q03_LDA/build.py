5ae480a4d5e4f8d251ece12bba8c650cfe4b8269
pranaliangre <pranali.r.c@gmail.com>
2018-03-14 17:26:47 +0000

    Done

--- q03_LDA/build.py
+++ q03_LDA/build.py
@@ -1,10 +1,16 @@
-# Default imports
 
 from sklearn.decomposition import LatentDirichletAllocation
 from greyatomlib.nlp_day_02_project.q02_count_vectorizer_for_LDA.build import q02_count_vectorizer_for_LDA
 
 
-# Write your solution here :
+def q03_LDA(path):
+
+    matrix, feature_names = q02_count_vectorizer_for_LDA(path)
+#     vocab = feature_names
+    model =LatentDirichletAllocation(n_topics=20, random_state=1,learning_method='batch',max_iter=500)
+    model.fit(matrix)
+    topic_word = model.components_
+    return topic_word
 
 
 
